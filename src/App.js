17c69339148e6cd843ae462ddd89e5a36df9be3a
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-10-25 20:34:30 -0500

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Search from "./components/search/Search";
 import Currentweather from "./components/current-weather/Currentweather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import './App.css';
-
-//23:42
 
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+      })
   }
   
   return (
